d03121cfc7532dd20eef34acd64dfce8cfe4da14
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-10-28 12:55:31 -0700

    🚀

---
+++ src/client/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <p>hello</p>
+        <p>world!</p>
+      </div>
+    );
+  }
+}
+
+export default App
