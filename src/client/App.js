b557655595ebdeb377ed3172c42055502caa0f51
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-10-28 14:04:53 -0700

    adds husky

--- src/client/App.js
+++ src/client/App.js
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
-import './app.css';
+import React, { Component } from "react";
+import "./app.css";
 
 class App extends Component {
   state = {
     name: "",
     result: ""
-  }
-
-  handleChange = e => {
-    this.setState({[e.target.name]:e.target.value})
   };
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   onSubmitForm = e => {
     e.preventDefault();
-    console.log("here")
-    fetch('/api')
+    fetch("/api")
       .then(res => res.json())
       .then(data => this.setState({ result: data.data }))
-      .catch(err => console.error(err))
-  }
-  
+      .catch(err => console.error(err));
+  };
+
   render() {
-    const { name, result } = this.state
-    
+    const { name, result } = this.state;
+
     return (
       <div>
         <form onSubmit={this.onSubmitForm}>
           <label htmlFor="name">
             Name:
-            <input type="text" name="name" id="name" value={name} />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={this.handleChange}
+            />
           </label>
           <input type="submit" value="Submit" />
         </form>
-        {result && (
-          <p>{result}</p>
-          )}
+        {result && <p>{result}</p>}
       </div>
     );
   }
 }
 
-export default App
+export default App;
