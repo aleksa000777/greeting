7e55bda7289bc8932c251c9f6a78682b22981a90
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-10-28 18:39:26 -0700

    adds temp local cache

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
+const Cache = require("./cache.js");
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api', (req, res) => res.send({ data: "hello" }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.use(express.static("dist"));
+
+// TODO temp local cache, replace with Redis
+const localCache = new Cache(20);
+
+// temp func replace with server call
+const getTimeZone = hours => {
+  if (hours < 12) return "Good Morning";
+  if (hours < 18) return "Good Afternoon";
+  return "Good Evening";
+};
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+  const hours = date.getHours();
+  const timeZone = getTimeZone(hours);
+  const user = localCache.get(req.query.name);
+  if (user !== timeZone) {
+    localCache.put(req.query.name, timeZone);
+  }
+  res.send({ data: `${timeZone} ${req.query.name}!` });
+});
+
+app.listen(8080, () => console.log("Listening on port 8080!"));
